d0ef5cb7705b3b9411f82c27711f03e938c63106
Ronan O'Cuinn <rocuinn@gmail.com>
2025-01-28 12:18:48 +0000

    Initial React app commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Ronan O'Cuinn
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Ronan O'Cuinn/i);
+  expect(linkElement).toBeInTheDocument();
+});
